ea84ec2990cb3a41257dc0905d44225ca5d99c74
Emanuel Martínez <emartinezpinzon@gmail.com>
2018-01-02 18:28:13 -0500

    Campos del modelo Post sin relaciones

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $table = "posts";
+
+    protected $fillable = [
+        'title', 'content', 'image', 'author_id', 'created_at',
+    ];
+    
+    protected $hidden = [
+        'updated_at',
+    ];
 }
